459e74b0617669bfc6aa21015b7339bae53af40b
Gerardo Nava <gerardo.nava.pereda@gmail.com>
2022-08-15 21:43:55 -0500

    ejercicios tema: condicionales

--- 04-Estructuras-de-control-condicional/calificaciones.js
+++ 04-Estructuras-de-control-condicional/calificaciones.js
@@ -1,9 +1,22 @@
-// 80-100 A
+// 90-100 A
 // 70-89 B
 // 60-69 C
 // 50-59 D
 // 0-49 F
 
 // Cuál es tu puntaje?
+const puntaje = 50;
 
 // Usar una estructura de control condicional para saber que letra corresponde a su puntaje
+// if () {} else if () {} else {}
+if ((puntaje >= 90 && puntaje <= 100) || puntaje === 1000) {
+  // intrucciones
+} else if (puntaje >= 70 && puntaje <= 89) {
+  // intrucciones
+} else if (puntaje >= 60 && puntaje <= 69) {
+  // intrucciones
+} else if (puntaje >= 50 && puntaje <= 59) {
+  // instrucciones
+} else {
+  // F
+}
