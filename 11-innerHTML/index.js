d2f1620989f2bcf3ce5445ad359ec44f6ebff387
cchavezmx99 <carlos.chavez@99minutos.com>
2022-09-01 01:32:58 +0000

    locastorage class

--- 11-innerHTML/index.js
+++ 11-innerHTML/index.js
@@ -10,5 +10,16 @@ function agregarHijo() {
   const input = document.getElementById('text');
   const texto = input.value;
   padre.innerHTML = hijos += liComponente(texto);
+  // array o objeto JSON.stringify para convertirlo en string
+  localStorage.setItem('hijos', hijos);
   input.value = '';
 }
+
+addEventListener('DOMContentLoaded', function(){
+  // para pasar de un string a un array o objeto JSON.parse
+  const hijosGuardados = localStorage.getItem('hijos')
+    if (hijosGuardados) {
+      hijos = hijosGuardados
+      padre.innerHTML = hijos
+    }
+})
